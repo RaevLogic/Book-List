b703a22f051001f058ee883b45a6f9f098daf7a0
Roger K <raevlogic@gmail.com>
2018-09-26 12:07:38 -0700

    Finished App

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'Eloquent Javascript', pages: 101},
+        { title: 'Harry Potter', pages: 606},
+        { title: 'Star Wars', pages: 1000},
+        { title: 'Scott Pilgrim', pages: 222}
+    ]
+}
